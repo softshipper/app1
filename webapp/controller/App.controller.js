ccbbc6c4cebb93d04163ea2e9797df9b171b1dbe
Anujit Marty <anujit.marty@swisslife.ch>
2021-01-22 12:33:20 +0000

    [MINOR] Init

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,85 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/ResizeHandler",
+	"sap/ui/core/mvc/Controller",
+	"sap/f/FlexibleColumnLayout"
+], function (JSONModel, ResizeHandler, Controller, FlexibleColumnLayout) {
+	"use strict";
+
+	return Controller.extend("io.example.app1.controller.App", {
+		onInit: function () {
+			this.oRouter = this.getOwnerComponent().getRouter();
+			this.oRouter.attachRouteMatched(this.onRouteMatched, this);
+			this.oRouter.attachBeforeRouteMatched(this.onBeforeRouteMatched, this);
+		},
+
+		onBeforeRouteMatched: function (oEvent) {
+
+			var oModel = this.getOwnerComponent().getModel();
+			var sLayout = oEvent.getParameters().arguments.layout;
+
+			// If there is no layout parameter, query for the default level 0 layout (normally OneColumn)
+			if (!sLayout) {
+				var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(0);
+				sLayout = oNextUIState.layout;
+			}
+
+			// Update the layout of the FlexibleColumnLayout
+			if (sLayout) {
+				oModel.setProperty("/layout", sLayout);
+			}
+
+		},
+
+		_updateLayout: function (sLayout) {
+			var oModel = this.getOwnerComponent().getModel();
+
+			// If there is no layout parameter, query for the default level 0 layout (normally OneColumn)
+			if (!sLayout) {
+				var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(0);
+				sLayout = oNextUIState.layout;
+			}
+
+			// Update the layout of the FlexibleColumnLayout
+			if (sLayout) {
+				oModel.setProperty("/layout", sLayout);
+			}
+		},
+
+		onRouteMatched: function (oEvent) {
+			var sRouteName = oEvent.getParameter("name"),
+				oArguments = oEvent.getParameter("arguments");
+
+			this._updateUIElements();
+
+			// Save the current route name
+			this.currentRouteName = sRouteName;
+		},
+
+		onStateChanged: function (oEvent) {
+			var bIsNavigationArrow = oEvent.getParameter("isNavigationArrow"),
+				sLayout = oEvent.getParameter("layout");
+
+			this._updateUIElements();
+
+			// Replace the URL with the new layout if a navigation arrow was used
+			if (bIsNavigationArrow) {
+				this.oRouter.navTo(this.currentRouteName, {
+					layout: sLayout
+				}, true);
+			}
+		},
+
+		// Update the close/fullscreen buttons visibility
+		_updateUIElements: function () {
+			var oModel = this.getOwnerComponent().getModel();
+			var oUIState = this.getOwnerComponent().getHelper().getCurrentUIState();
+			oModel.setData(oUIState);
+		},
+
+		onExit: function () {
+			this.oRouter.detachRouteMatched(this.onRouteMatched, this);
+			this.oRouter.detachBeforeRouteMatched(this.onBeforeRouteMatched, this);
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"io/example/app1/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
